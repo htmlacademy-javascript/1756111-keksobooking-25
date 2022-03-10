e68e79e17a8b515ec914bf4fb5d7f594631fe240
Danila <Gdanila2002@yandex.ru>
2022-03-10 11:54:49 +0300

    Сделал первую функцию

--- js/main.js
+++ js/main.js
@@ -1,20 +1,19 @@
-let min;
-let max;
-let fraction;
-let random;
-
-function randomNumber(min, max) { //Случайное число от / до
-  if (min>=0 && max>=0 && min > max) { // Если у нас мин и макс неотрицательные и минимум не больше макса, выполняется условие
-    random = Math.floor(Math.random()*(max - min))+min; //Я не совсем понимаю принцип выполнения, но в лерн жс и девелопер так считается случайное число
+const getRandomIntegerNumber = (min, max) => {
+  if (min < 0 || max < 0 || min >= max) {
+    return 'Неверный диапазон переданных чисел'
   }
-  console.log(random) // выводим случайное число
-}
 
-function degreeRandomNumber(min, max, fraction) {  //Случайное число от / до / знаков после запятой
-  if (min>=0 && max>=0 && min > max && fraction >= 0) { // Если у нас мин и макс неотрицательные и минимум не больше макса и знаков больше нуля, выполняется условие
-    random = Math.floor(Math.random()*(max - min))+min; // Я не понимаю, как указать, сколько разрешено нулей после запятой
+  const random = Math.floor(Math.random() * (max - min)) + min;
+  return random;
+};
 
-    // была идея взять fraction = (Math.pow (10,fraction)) и посчитать сколько нулей, но я смотрел в гугле, там вообще неизвестные для меня команды
-    console.log(random) // выводим случайное число
+getRandomIntegerNumber(1, 10);
+
+function degreeRandomNumber(min, max, fraction) {
+  if (min>=0 && max>=0 && min > max && fraction >= 0) {
+    random = Math.floor(Math.random()*(max - min))+min;
+
+
+    console.log(random)
   }
 }
