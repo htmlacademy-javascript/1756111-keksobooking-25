3e96b3d14a14d427bb41a229691857219889ba3b
Danila <Gdanila2002@yandex.ru>
2022-04-07 11:44:39 +0300

    Работа над ошибками

--- js/popup.js
+++ js/popup.js
@@ -12,9 +12,9 @@ const renderCard = (ad) => {
   adElement.querySelector('.popup__type').textContent = ad.offer.type;
   adElement.querySelector('.popup__text--capacity').textContent = ad.offer.checkin;
   adElement.querySelector('.popup__text--time').textContent = ad.offer.checkout;
-  adElement.querySelector('.popup__features').textContent = ad.offer.features;
+  adElement.querySelector('.popup__features').textContent = ad.offer.features; //TODO тут небольшие проблемы
   adElement.querySelector('.popup__description').textContent = ad.offer.description;
-  adElement.querySelector('.popup__photos').href = ad.offer.photos; //TODO тут небольшие проблемы
+  adElement.querySelector('.popup__photos').append = ad.offer.photos; //TODO тут небольшие проблемы
   adElement.querySelector('.popup__avatar').href = ad.author.avatar;
 
   return adElement;
