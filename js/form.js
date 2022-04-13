5aa2f4774906f6ecb239127b90cf2f62ac4c5957
Danila <Gdanila2002@yandex.ru>
2022-04-13 06:16:52 +0300

    Выполнил задание 8

--- js/form.js
+++ js/form.js
@@ -1,5 +1,11 @@
 const formTemplate = document.querySelector('.ad-form');
 const mapFiltersTemplate = document.querySelector('.map__filters');
+const typeFlat = formTemplate.querySelector('select[name=type]');
+const pricePerNight = formTemplate.querySelector('input[name=price]');
+const timeIn = formTemplate.querySelector('select[name=timein]');
+const timeOut = formTemplate.querySelector('select[name=timeout]');
+const numberRooms = formTemplate.querySelector('select[name=rooms]');
+const numberGuests = formTemplate.querySelector('select[name=capacity]');
 
 const activate = () => {
   formTemplate.classList.remove('ad-form--disabled');
@@ -14,6 +20,83 @@ const deactivate = () => {
   formTemplate.querySelectorAll('fieldset').forEach(item => item.disabled = true);
   mapFiltersTemplate.querySelectorAll('select, fieldset').forEach(item => item.disabled = true);
 };
+
+
+// тип жилья - мин цена
+const minPrice = {
+  bungalow: 0,
+  flat: 1000,
+  hotel: 3000,
+  house: 5000,
+  palace: 10000
+};
+
+const setMinPrice = () => {
+  const value = minPrice[typeFlat.value];
+
+  pricePerNight.setAttribute('min', value);
+  pricePerNight.setAttribute('placeholder', value);
+};
+
+// заезд - выезд
+const toSyncTimeOut = () => {
+  timeOut.value = timeIn.value;
+};
+
+const toSyncTimeIn = () => {
+  timeIn.value = timeOut.value;
+};
+
+// колво комнат - колво гостей
+const checkNumberOfGuestsAndRooms = () => {
+  const roomsValue = parseInt(numberRooms.value, 10);
+  const guestsValue = parseInt(numberGuests.value, 10);
+  console.log(roomsValue, guestsValue)
+
+  if (roomsValue === 1 && (guestsValue > 1 || guestsValue === 0)) {
+    numberGuests.setCustomValidity('Для 1 гостя');
+  } else if (roomsValue === 2 && (guestsValue > 2 || guestsValue === 0)) {
+    numberGuests.setCustomValidity('Для 2 или 1 гостя');
+  } else if (roomsValue === 3 && guestsValue === 0) {
+    numberGuests.setCustomValidity('Для 3 или 2 или 1 гостя');
+  } else if (roomsValue === 100 && guestsValue !== 0) {
+    numberGuests.setCustomValidity('Не для гостей');
+  } else {
+    numberGuests.setCustomValidity('')
+  }
+
+  // if (roomsValue !== 100 && guestsValue === 0) {
+  //   numberGuests.setCustomValidity('Недостаточно гостей');
+  // } else if (roomsValue < guestsValue) {
+  //   numberGuests.setCustomValidity('Гостей очень много');
+  // } else if (roomsValue === 100 && guestsValue !== 0) {
+  //   numberGuests.setCustomValidity('Данный вариант не для гостей');
+  // } else {
+  //   numberGuests.setCustomValidity('');
+  // }
+};
+
+// навешиваем обработчики
+numberRooms.addEventListener('change', () => {
+  checkNumberOfGuestsAndRooms();
+});
+
+numberGuests.addEventListener('change', () => {
+  checkNumberOfGuestsAndRooms();
+});
+
+typeFlat.addEventListener('change', () => {
+  setMinPrice();
+});
+
+timeIn.addEventListener('change', () => {
+  toSyncTimeOut();
+});
+
+timeOut.addEventListener('change', () => {
+  toSyncTimeIn();
+});
+
 
 // deactivate();
 
